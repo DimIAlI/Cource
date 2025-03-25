f40ae4a21c60ee29b62660a63b2d4ad75d44e72e
DimIAlI <dimir.aliev1994@gmail.com>
2025-03-25 22:12:14 +0400

    Removed custom class loader and related functionality

--- CoworkingSpaceReservation/src/main/java/org/example/Main.java
+++ CoworkingSpaceReservation/src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import lombok.SneakyThrows;
-import org.example.util.ProgramRunnerInvoker;
 
 public class Main {
 
@@ -12,7 +11,7 @@ public class Main {
 
     @SneakyThrows
     private void start() {
-        ProgramRunnerInvoker.initialize("lib");
-        ProgramRunnerInvoker.run();
+        ProgramRunner runner = ProgramRunner.createRunner();
+        runner.run();
     }
 }
